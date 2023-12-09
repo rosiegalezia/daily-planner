3ff6855529ff5e9dd6c2021d8a89e449fc40515e
rosiegalezia <rosegalezia@gmail.com>
2023-12-09 23:31:46 +0000

    pseudocoded and began creating elements

---
+++ Assets/script.js
@@ -0,0 +1,38 @@
+{/* <h1 class="display-3">Work Day Scheduler</h1>
+<p class="lead">A simple calendar app for scheduling your work day</p>
+<p id="currentDay" class="lead"></p> */}
+
+var title = document.getElementsByClassName(".display-3")
+var currentDay = document.getElementById("#currentDay")
+var currentTime = document.getElementById("#currentTime")
+
+var container = document.getElementById("#container")
+var blockElements = container.children
+var textArea = document.getElementsByClassName(".textArea")
+
+// make currentDay display the date and time
+currentDay.textContent = dayjs()
+
+// loop through all the blocks
+for (var i=0; i<blockElements.length; i++){
+
+}
+
+
+
+// have timeblock colors correspond to past, present, future
+    // get current time hour via dayjs
+    // loop over all the timeblocks, conditionally assign past present or future class (see css) by comparing that timeblock's hour to the current hour
+
+
+ 
+ //saving timeblock text in local storage
+    // loop over all the timeblocks and get the current value of the textarea
+    // decide how you want to store the text for each hour in local storage
+        // can either store each individual hour as its own key 
+        // (e.g. in local storage, hour-9: "something") 
+
+
+
+// retrieve the save values from local storage and display them inside each timeblock whenever the page reloads 
+// if you stored as individual keys, get each hour value from local storage and display in the corresponding textarea
