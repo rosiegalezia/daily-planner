e404c2dffc73a1b6cd309c6eac65c27033f7608e
rosiegalezia <rosegalezia@gmail.com>
2023-12-10 14:28:54 +0000

    Added interval function to make the time displayed at the top update every second. Began defining breakpoints for dayjs to format the timeblock colours

--- Assets/script.js
+++ Assets/script.js
@@ -6,27 +6,38 @@ var title = document.getElementsByClassName(".display-3")
 // var currentDay = document.querySelector("#currentDay")
 // var currentTime = document.getElementById("#currentTime")
 
-var container = document.getElementById("#container")
-// var blockElements = container.children
 var textArea = document.getElementsByClassName(".textArea")
+
+// count the number of blocks
+let blockCount = container.childElementCount;
+console.log(blockCount)
+
+// define the hour breakpoints to later compare to time
+var hourOne = dayjs("09:00:01")
+var hourTwo = dayjs("10:00:01")
+var hourThree = dayjs("11:00:01")
+var hourFour = dayjs("12:00:01")
+var hourFive = dayjs("13:00:01")
+
+
+var // Sets timer to check time every second
+time = setInterval(function() {
 
 // make currentDay display the date and time
 // referesh every ??? so it's up to date
-
 var today = dayjs().format("dddd D MMM YYYY")
 console.log(today)
 $("#currentDay").text(today)
 
-var time = dayjs().format("H:mm:ss");
+var time = dayjs().format("HH:mm:ss");
 $("#currentTime").text(time)
 
+}, 1000)
 
+// loop through all the blocks
+for (var i=0; i < blockCount; i++){
 
-// // loop through all the blocks
-// for (var i=0; i<blockElements.length; i++){
-
-// }
-
+}
 
 
 // have timeblock colors correspond to past, present, future
@@ -45,3 +56,7 @@ $("#currentTime").text(time)
 
 // retrieve the save values from local storage and display them inside each timeblock whenever the page reloads 
 // if you stored as individual keys, get each hour value from local storage and display in the corresponding textarea
+
+// clear the blocks at the end of the day
+// if time>23.59,
+// textArea.text=""
