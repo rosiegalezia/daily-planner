2a4bf82547e9732636bd763db8e2b8b1b4f0d631
rosiegalezia <rosegalezia@gmail.com>
2023-12-11 19:46:47 +0000

    added placeholder text in each box, added some responsiveness to the blocks, troubleshooting saving textarea content to local storage and the conditional formatting

--- Assets/script.js
+++ Assets/script.js
@@ -1,17 +1,3 @@
-{/* <h1 class="display-3">Work Day Scheduler</h1>
-<p class="lead">A simple calendar app for scheduling your work day</p>
-<p id="currentDay" class="lead"></p> */}
-
-var title = $(".display-3")
-
-var textArea = $(".textArea")
-
-// var block910 = $("#block1")
-
-// count the number of blocks
-let blockCount = container.childElementCount;
-console.log(blockCount)
-
 // define the hour breakpoints to later compare to time
 var hourOne = dayjs("09:00:01")
 var hourTwo = dayjs("10:00:01")
@@ -20,27 +6,32 @@ var hourFour = dayjs("12:00:01")
 var hourFive = dayjs("13:00:01")
 
 
+// var block1 = $("#block1.textArea")
+// var block1content = block1.val()
+// var block2 = $("#block2.textArea")
+
+
+// count the number of blocks
+let blockCount = container.childElementCount;
+console.log(blockCount)
+// do i need an array of blocks????????
+
+
 var // Sets timer to check time every second
-time = setInterval(function() {
+    time = setInterval(function () {
 
-// make currentDay display the date
-var today = dayjs().format("dddd D MMM YYYY")
-// console.log(today)
-$("#currentDay").text(today)
+        // make currentDay display the date
+        var today = dayjs().format("dddd D MMM YYYY")
+        // console.log(today)
+        $("#currentDay").text(today)
 
-// make currentTime display the time
-var time = dayjs().format("HH:mm:ss");
-$("#currentTime").text(time)
+        // make currentTime display the time
+        var time = dayjs().format("HH:mm:ss");
+        $("#currentTime").text(time)
 
-}, 1000)
+    }, 1000)
 
-// // loop through all the blocks
-// for (var i=0; i < blockCount; i++){
-// }
-// ------------------------------------------------
 
-// set all the timeblocks to future by default
-$(".time-block").addClass("future")
 
 // get the current time (hour block) via dayjs
 var currentHour = dayjs().format("HH")
@@ -51,46 +42,70 @@ console.log(currentHour)
 
 //loop through all the blocks
 
-for (var i=0; i < blockCount; i++){
+// while (8 < currentHour < 19){}
 
-if (9 < currentHour < 10){
-    $("#block1").removeClass("future").addClass("present")
-} else
+for (var i = 0; i < blockCount; i++) {
 
-if (10 < currentHour < 11){
-    $("#block1").addClass("past")
-    $("#block2").removeClass("future").addClass("present")
-} else
+    // set all the timeblocks to future by default
+    $(".time-block").addClass("future")
 
-if (11 < currentHour < 12){
-    $("#block1 block2").addClass("past")
-    $("#block3").removeClass("future").addClass("present")
-} else
+    if (9 <= currentHour < 10) {
+        $("#block1").removeClass("future").addClass("present")
+    } else
 
-if (12 < currentHour < 13){
-    $("#block1 block2 block3").addClass("past")
-    $("#block4").removeClass("future").addClass("present")
-} else
+        if (10 <= currentHour < 11) {
+            $("#block1").addClass("past")
+            $("#block2").removeClass("future").addClass("present")
+        } else
 
-if (13 < currentHour < 14){
-    $("#block1 block2 block3 block4").addClass("past")
-    $("#block5").removeClass("future").addClass("present")
+            if (11 <= currentHour < 12) {
+                $("#block1 block2").addClass("past")
+                $("#block3").removeClass("future").addClass("present")
+            } else
+
+                if (12 <= currentHour < 13) {
+                    $("#block1 block2 block3").addClass("past")
+                    $("#block4").removeClass("future").addClass("present")
+                } else
+
+                    if (13 <= currentHour < 14) {
+                        $("#block1 block2 block3 block4").addClass("past")
+                        $("#block5").removeClass("future").addClass("present")
+                    }
 }
-}
 
+//saving timeblock text in local storage
+// loop over all the timeblocks and get the current value of the textarea
+// decide how you want to store the text for each hour in local storage
+// can either store each individual hour as its own key 
+// (e.g. in local storage, hour-9: "something")
 
- 
- //saving timeblock text in local storage
-    // loop over all the timeblocks and get the current value of the textarea
-    // decide how you want to store the text for each hour in local storage
-        // can either store each individual hour as its own key 
-        // (e.g. in local storage, hour-9: "something") 
+// // testing the button event listener
+// $(".saveBtn").on("click", function(){
+//     console.log(textArea)
+//     // $("#block1").removeClass("future").addClass("present")
+//     // $("#block2").removeClass("future").addClass("present")
+// })
 
+// $(".saveBtn").on("click", setBlockContent())
 
+// // set timeblock content
+// function setBlockContent() {
+//     var block1 = $("#block1.textArea")
+//     var block1content = block1.val()
+//     localStorage.setItem("block1", JSON.stringify(block1content))
+//     console.log(localStorage)
+// }
 
-// retrieve the save values from local storage and display them inside each timeblock whenever the page reloads 
-// if you stored as individual keys, get each hour value from local storage and display in the corresponding textarea
+// // retrieve the save values from local storage and display them inside each timeblock whenever the page reloads
+// // if you stored as individual keys, get each hour value from local storage and display in the corresponding textarea
+
+// // get timeblock content
+// function getBlockContent() {
+//     var block1content = JSON.parse(localStorage.getItem("block1"))
+// }
 
 // clear the blocks at the end of the day
-// if time>23.59,
-// textArea.text=""
+// if (19 < currentHour) {
+//     textArea.text = ""
+// }
